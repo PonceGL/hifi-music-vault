5190d5f07c1075d1202e738c5bd0bc1c2fe2781c
PonceGL <poncianogl@hotmail.com>
2026-02-08 11:18:48 -0600

    feat: implement file dropzone functionality on SetupPage and remove custom styling from index.css.

--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -1,7 +1,35 @@
+import { Dropzone, DropzoneContent, DropzoneEmptyState } from "@/components/ui/dropzone"
+import { useState } from "react"
+
 export function SetupPage() {
+    const [files, setFiles] = useState<File[]>()
+
     return (
-        <div>
+        <main className="w-full flex flex-col justify-start items-center">
             <h1 className="text-3xl font-bold underline">Setup</h1>
-        </div>
+            <div className="w-full p-8 flex items-center justify-center gap-20">
+                <Dropzone
+                    src={files}
+                    onDrop={acceptedFiles => setFiles(acceptedFiles)}
+                    accept={{ "image/*": [".png", ".jpg", ".jpeg", ".gif"] }}
+                    maxSize={5 * 1024 * 1024}
+                    className="w-full max-w-md"
+                >
+                    <DropzoneContent />
+                    <DropzoneEmptyState />
+                </Dropzone>
+
+                <Dropzone
+                    src={files}
+                    onDrop={acceptedFiles => setFiles(acceptedFiles)}
+                    accept={{ "image/*": [".png", ".jpg", ".jpeg", ".gif"] }}
+                    maxSize={5 * 1024 * 1024}
+                    className="w-full max-w-md"
+                >
+                    <DropzoneContent />
+                    <DropzoneEmptyState />
+                </Dropzone>
+            </div>
+        </main>
     )
 }
