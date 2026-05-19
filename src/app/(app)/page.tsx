0084993a6322765b172db7f0657e453e8c5cf3b6
PonceGL <poncianogl@hotmail.com>
2026-05-19 16:44:44 -0600

    feat: update layout pages with title headings and adjust onboarding container width

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,9 @@
 import type { ReactElement } from "react";
 
 export default function LibraryPage(): ReactElement {
-  return <div />;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Library</h1>
+    </div>
+  );
 }
--- src/app/(app)/playlists/page.tsx
+++ src/app/(app)/playlists/page.tsx
@@ -1,5 +1,9 @@
 import type { ReactElement } from "react";
 
 export default function PlaylistsPage(): ReactElement {
-  return <div />;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Playlists</h1>
+    </div>
+  );
 }
