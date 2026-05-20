638f6b4f90b3f28cf2540c745f2d2ce9a05c8a61
PonceGL <poncianogl@hotmail.com>
2026-05-20 16:54:33 -0600

    fix: polyfill TextDecoder/TextEncoder in jest.setup for jsdom environment

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom";
+import { TextDecoder, TextEncoder } from "util";
+
+// jsdom does not expose TextDecoder/TextEncoder as globals; polyfill from Node.
+Object.assign(global, { TextDecoder, TextEncoder });
