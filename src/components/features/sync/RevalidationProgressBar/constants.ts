32ef76af4f2e7fdea209f2bb0ab2c4152367961f
PonceGL <poncianogl@hotmail.com>
2026-05-25 13:40:02 -0600

    feat(MFM-499): add RevalidationProgressBar component

---
+++ src/components/features/sync/RevalidationProgressBar/constants.ts
@@ -0,0 +1,4 @@
+export const REVALIDATION_BAR_ARIA_LABEL = "Actualizando biblioteca";
+export const REVALIDATION_BAR_LABEL = "Actualizando biblioteca...";
+export const REVALIDATION_BAR_PROGRESS_ARIA_LABEL =
+  "Progreso de re-validación de biblioteca";
